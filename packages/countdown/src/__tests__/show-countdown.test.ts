21a2b26f5fb74a3f7d25ee555690f5bcc91cb64f
Paul Couthouis <couthouis.p@baseconnect.in>
2022-08-19 20:17:32 +0900

    init countdown package

---
+++ packages/countdown/src/__tests__/show-countdown.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest'
+import { show } from '../countdown'
+
+test('This is a test', () => {
+  expect(show()).toBe('ok')
+})
---
+++ packages/countdown/src/countdown.ts
@@ -0,0 +1,2 @@
+const str = 'ok'
+export const show = () => str
