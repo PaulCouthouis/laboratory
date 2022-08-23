228c2ebe6227e39efa6a72bceb2ef36128f17910
Paul Couthouis <couthouis.p@baseconnect.in>
2022-08-23 20:42:11 +0900

    wcc: init countdown

--- packages/wcc/src/App.svelte
+++ packages/wcc/src/App.svelte
@@ -1,45 +1,12 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+import Countdown from "./components/Countdown.svelte";
 </script>
 
+<h1 class="mb-4">Web Components Collection</h1>
+
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <article class="border p-1 flex flex-col items-center">
+    <h2 class="mt-0 mb-2">Countdown</h2>
+    <Countdown />
+  </article>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
