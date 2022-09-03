b41a203fbf7577bf7095314612c88947e86342c6
Paul Couthouis <couthouis.p@baseconnect.in>
2022-09-03 16:33:52 +0900

    mariage: countdown

---
+++ packages/apps/mariage/src/components/Countdown/Countdown.svelte
@@ -0,0 +1,22 @@
+<script lang='ts'>
+  import { init } from 'countdown/src/adapters/nanostores'
+  import TimeBlock from './child/TimeBlock.svelte'
+  
+  export let finalTime: Date
+
+  const getNow = () => new Date()
+
+  const loop = (callback: (startTime: Date) => void) => {
+    callback(getNow())
+    setTimeout(() => loop(callback), 1000)
+  }
+
+  $: countdown = init(loop, new Date(finalTime))
+</script>
+
+<p class="flex flex-wrap justify-around">
+  <TimeBlock labelTime={$countdown.days} description='Jours' />
+  <TimeBlock labelTime={$countdown.hours} description='Heures' />
+  <TimeBlock labelTime={$countdown.minutes} description='Minutes' />
+  <TimeBlock labelTime={$countdown.seconds} description='Secondes' />
+</p>
---
+++ packages/apps/mariage/src/components/Countdown/child/TimeBlock.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let labelTime: number
+  export let description: string
+</script>
+
+<span class="my-4 flex flex-col mx-3 items-center">
+  <span class="flex flex-wrap justify-center items-center  text-white text-2xl font-bold">{labelTime}</span> 
+  <span class="my-2 uppercase font-bold">{description}</span>
+</span>
