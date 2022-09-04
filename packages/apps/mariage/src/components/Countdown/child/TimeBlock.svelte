7ed638752857913d95171bed338775288841c8e6
Paul Couthouis <couthouis.p@baseconnect.in>
2022-09-04 15:17:53 +0900

    mariage: add place

--- packages/apps/mariage/src/components/Countdown/child/TimeBlock.svelte
+++ packages/apps/mariage/src/components/Countdown/child/TimeBlock.svelte
@@ -3,7 +3,7 @@
   export let description: string
 </script>
 
-<span class="my-4 flex flex-col mx-3 items-center">
+<span class="min-w-[35%] sm:min-w-0 my-4 flex flex-col mx-3 items-center">
   <span class="flex flex-wrap justify-center items-center  text-white text-2xl font-bold">{labelTime}</span> 
   <span class="my-2 uppercase font-bold">{description}</span>
 </span>
