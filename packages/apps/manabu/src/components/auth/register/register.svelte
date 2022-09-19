4ab7c9b1eea50a89951e85d771323b2aa210b646
Paul Couthouis <couthouis.p@baseconnect.in>
2022-09-19 15:41:46 +0900

    manabu: register component step 1

---
+++ packages/apps/manabu/src/components/auth/register/register.svelte
@@ -0,0 +1,48 @@
+<script lang="ts"></script>
+
+<form>
+  <div class="form-control">
+    <label class="label" for="nickname">
+      <span class="label-text">Pseudo / Surnom *</span>
+    </label>
+    <input 
+      required
+      minlength="1"
+      maxlength="255" 
+      class="input" 
+      id="nickname" 
+      placeholder="Pikachu" 
+      type="text"
+    />
+  </div>
+  <div class="form-control">
+    <label class="label" for="email">
+      <span class="label-text">Email *</span>
+    </label>
+    <input 
+      required
+      minlength="5"
+      maxlength="255" 
+      class="input" 
+      id="email" 
+      placeholder="pika@pi.ka" 
+      type="email"
+    />
+  </div>
+  <div class="form-control">
+    <label class="label" for="password">
+      <span class="label-text">Mot de passe *</span>
+    </label>
+    <input 
+      required 
+      minlength="8"
+      maxlength="255" 
+      class="input" 
+      id="password"
+      type="password"
+    />
+  </div>
+  <div class="form-control mt-6">
+    <button class="btn btn-primary" type="submit">S'inscrire</button>
+  </div>
+</form>
