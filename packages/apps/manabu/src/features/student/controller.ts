67efd5559ee340dc05883d29b04de13af2d5dfbf
Paul Couthouis <couthouis.p@baseconnect.in>
2022-09-30 19:20:56 +0900

    manabu: move files

--- packages/apps/manabu/src/features/student/controller.ts
+++ packages/apps/manabu/src/features/student/controller.ts
@@ -1,5 +1,5 @@
 import { supabase } from '../../database/supabase'
-import { createStudentRepositoryInSupabase } from './adapter/supabase'
+import { createStudentRepositoryInSupabase } from './gateway/supabase'
 import { createStudentInteractor } from './domain/interactor'
 
 const repository = createStudentRepositoryInSupabase(supabase.auth)
