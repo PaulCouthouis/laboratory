05932277f0c5e7cb704e2ffe22f91350bf1a69b8
Paul Couthouis <couthouis.p@baseconnect.in>
2022-10-15 13:23:35 +0900

    tangodex: esling config

---
+++ packages/apps/tangodex/.eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'functional'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:astro/recommended',
+    'plugin:functional/external-recommended',
+    'plugin:functional/recommended',
+    'plugin:functional/stylistic',
+  ],
+  ignorePatterns: ['*.cjs'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+      rules: {
+        'astro/no-set-html-directive': 'error',
+      },
+    },
+  ],
+}
