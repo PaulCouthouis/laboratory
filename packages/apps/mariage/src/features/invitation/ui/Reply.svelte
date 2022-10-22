ec42c6561082801ac6837fad2e2e4017379150af
Paul Couthouis <couthouis.p@baseconnect.in>
2022-10-22 14:35:48 +0900

    mariage: loading and thank

--- packages/apps/mariage/src/features/invitation/ui/Reply.svelte
+++ packages/apps/mariage/src/features/invitation/ui/Reply.svelte
@@ -2,7 +2,9 @@
   import type { ReplyCard, ReplyCardMember } from "../domain/values"
 
   export let members: string[]
-  
+
+  let completed = false
+  let loading = false
   let message = ''
   let cardMembers = members.map<ReplyCardMember>(member => ({
     diet: '',
@@ -15,11 +17,14 @@
       members: cardMembers,
       message
     }
-    console.log(body)
+
+    loading = true
 
     fetch('/api/reply', {
       body: JSON.stringify(body),
       method: 'POST'
+    }).then(() => {
+      completed = true
     })
   }
 </script>
@@ -30,31 +35,43 @@
       <legend class="text-center">- {card.name} -</legend>
       <div class="flex justify-between my-2">
         <label>
-          <input type="radio" bind:group={card.status} name={card.name} value={'accept'}  /> Présent
+          <input disabled={completed} type="radio" bind:group={card.status} name={card.name} value={'accept'}  /> Présent
         </label>
         <label>
-          <input type="radio" bind:group={card.status} name={card.name} value={'decline'} /> Absent
+          <input disabled={completed} type="radio" bind:group={card.status} name={card.name} value={'decline'} /> Absent
         </label>
         <label>
-          <input type="radio" bind:group={card.status} name={card.name} value={'wait'} /> Indécis
+          <input disabled={completed} type="radio" bind:group={card.status} name={card.name} value={'wait'} /> Indécis
         </label>
       </div>
       <input
-        class="border p-2 w-full text-gray-700"
+        class="border p-2 w-full text-gray-700 disabled:text-gray-700"
         placeholder="Régime alimentaire* (facultatif)"
         type="text"
         bind:value={card.diet}
+        disabled={completed} 
       />
     </fieldset>
   {/each}
   <textarea
-    class="border h-28 p-2 w-full text-gray-700"
+    class="border h-28 p-2 w-full text-gray-700 disabled:text-gray-700"
     placeholder="Message (facultatif)"
     bind:value={message}
+    disabled={completed} 
   />
+  {#if completed}
+    <p class="text-right mt-5">
+      Merci pour votre réponse !
+    </p>
+  {:else}
   <div class="text-right mt-4">
-    <button class="text-right bg-primary rounded p-4" type="submit">
+    <button class="btn text-white text-right bg-primary rounded p-4" class:pl-9={!loading} class:loading={loading} type="submit">
       Envoyer le coupon-réponse
     </button>
   </div>
+  {/if}
 </form>
+<p class="text-xs text-gray-300 mt-2 text-right">
+  * alergie, végétarien, raison religieuse, etc
+</p>
+
