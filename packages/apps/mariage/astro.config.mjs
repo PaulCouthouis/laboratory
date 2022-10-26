1821627a65ea97f6792ea4e95a91a60e8a591590
Paul Couthouis <couthouis.p@baseconnect.in>
2022-10-26 22:32:37 +0900

    mariage: opti images

--- packages/apps/mariage/astro.config.mjs
+++ packages/apps/mariage/astro.config.mjs
@@ -3,9 +3,17 @@ import tailwind from '@astrojs/tailwind'
 import svelte from '@astrojs/svelte'
 import vercel from '@astrojs/vercel/serverless'
 
+import image from '@astrojs/image'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [
+    tailwind(),
+    svelte(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
   output: 'server',
   adapter: vercel(),
 })
