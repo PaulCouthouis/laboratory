c8f6e5e39bcaaa85da6f711fd9f92e4b4eaaf3db
Paul Couthouis <couthouis.p@baseconnect.in>
2022-11-11 18:21:16 +0900

    tangodex: fix airtable bases

--- packages/apps/tangodex/src/db/airtable/bases.ts
+++ packages/apps/tangodex/src/db/airtable/bases.ts
@@ -2,9 +2,14 @@ import Airtable from 'airtable'
 
 Airtable.configure({
   endpointUrl: 'https://api.airtable.com',
-  apiKey: import.meta.env.PUBLIC_AIRTABLE_API_KEY,
+  apiKey:
+    import.meta.env.PUBLIC_AIRTABLE_API_KEY ||
+    import.meta.env.VITE_AIRTABLE_API_KEY,
 })
 
-const base = Airtable.base(import.meta.env.PUBLIC_AIRTABLE_BASE_ID)
+const base = Airtable.base(
+  import.meta.env.PUBLIC_AIRTABLE_BASE_ID ||
+    import.meta.env.VITE_AIRTABLE_BASE_ID
+)
 
 export const WordCardsBase = base('Word Cards')
